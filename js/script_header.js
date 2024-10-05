646e1a7ea0c7ee78d5facf7393d883efbda1bda8
Sif-161 <netostruduth@gmail.com>
2024-10-05 01:37:48 -0300

    responsividade pronta

--- js/script_header.js
+++ js/script_header.js
@@ -1,7 +1,9 @@
-function cartHeader() {
+function headerMobile() {
     const header = document.querySelector('header');
+    console.log("Largura da janela:", window.innerWidth);
+    console.log("Caminho da URL:", window.location.pathname);
 
-    if (window.innerWidth <= 430) {
+    if (window.innerWidth <= 932 && window.location.pathname === '/pages/cart.html') {
         header.innerHTML = `
         <div class="container-menu">
             <ul class="menu">
@@ -32,9 +34,50 @@ function cartHeader() {
             </div>
         </div>
         `;
+    }else if(window.innerWidth <= 932 && window.location.pathname === '/index.html'){
+        header.innerHTML = `
+        <div class="container-menu">
+            <ul class="menu">
+                <li id="logo">
+                    <a href="../index.html">
+                        <img src="../images/logo.png" alt="logo">
+                    </a>
+                </li>
+                <li class="icons">
+                    <a href="../pages/contact.html" aria-label="Contato" title="Contato">
+                        <i class="bi bi-telephone-fill"></i>
+                    </a>
+                </li>
+                <li class="icons">
+                    <a href="../pages/cart.html" aria-label="Carrinho" title="Carrinho">
+                        <i class="bi bi-bag-fill"></i>
+                        <span class="cart-count"></span>
+                    </a>
+                </li>
+            </ul>
+            <div id="search">
+                <form action="/search" method="get">
+                    <input type="text" name="q" placeholder="Pesquisar..." autocomplete="off">
+                    <button type="submit">
+                        <i class="bi bi-search"></i>
+                    </button>
+                </form>
+            </div>
+        </div>
+        <nav>
+            <ul class="navbar">
+                <li><a href="#">Cachorros</a></li>
+                <li><a href="#">Gatos</a></li>
+                <li><a href="#">Peixes</a></li>
+                <li><a href="#">Outros Animais</a></li>
+                <li><a href="#">Plano de Saúde</a></li>
+                <li><a href="#">Serviços</a></li>
+            </ul>
+        </nav>
+        `;
     }
 }
 
-cartHeader();
+headerMobile();
 
 
